f93112e31638cd1c8747b86a4e324f91bf79df9f
Andreas Schneider <asn@cryptomilk.org>
2024-03-05 19:21:39 +0100

    Format code with prettier

--- grammar.js
+++ grammar.js
@@ -12,22 +12,12 @@ const ANYTHING = /[^\r\n]+/;
 module.exports = grammar({
     name: 'rpmspec',
 
-    extras: ($) => [
-        $.comment,
-        WHITE_SPACE
-    ],
+    extras: ($) => [$.comment, WHITE_SPACE],
 
-    inline: $ => [
-        $._special_variable_name,
-    ],
+    inline: ($) => [$._special_variable_name],
 
     rules: {
-        spec: ($) => repeat(
-            choice(
-                $.preamble,
-                NEWLINE,
-            )
-        ),
+        spec: ($) => repeat(choice($.preamble, NEWLINE)),
 
         comment: ($) => token(seq('#', optional(ANYTHING))),
 
@@ -38,59 +28,57 @@ module.exports = grammar({
         preamble: ($) => seq($.variable),
 
         // Name:   tree-sitter-rpmspec
-        variable: ($) => seq(
-            $.name,
-            /:[ ]+/,
-            field("value", $._value)
-        ),
+        variable: ($) => seq($.name, /:[ ]+/, field('value', $._value)),
 
-        name: ($) => choice(
-            'AutoProv',
-            'AutoReq',
-            'AutoReqProv',
-            'AutoRequires',
-            'BuildArch',
-            'BuildArchitectures',
-            'BuildConflicts',
-            'BuildPreReq',
-            'BuildRequires',
-            'BuildRoot',
-            'Conflicts',
-            'Distribution',
-            'Enhances',
-            'Epoch',
-            'ExcludeArch',
-            'ExclusiveArch',
-            'ExclusiveOS',
-            'Group',
-            'License',
-            'Name',
-            'Obsoletes',
-            'Packager',
-            'Prereq',
-            'Provides',
-            'Recommends',
-            'Release',
-            'Requires',
-            'Suggests',
-            'Summary',
-            'Supplements',
-            'URL',
-            'Url',
-            'Vendor',
-            'Version',
-            /Patch\d*/,
-            /Source\d*/,
-        ),
-        _value: ($) => choice(
-            $.simple_expansion,
-            $.expansion,
-            $.integer,
-            $.float,
-            seq($.float, optional($.simple_expansion)),
-            seq($.float, optional($.expansion)),
-            $.string,
-        ),
+        name: ($) =>
+            choice(
+                'AutoProv',
+                'AutoReq',
+                'AutoReqProv',
+                'AutoRequires',
+                'BuildArch',
+                'BuildArchitectures',
+                'BuildConflicts',
+                'BuildPreReq',
+                'BuildRequires',
+                'BuildRoot',
+                'Conflicts',
+                'Distribution',
+                'Enhances',
+                'Epoch',
+                'ExcludeArch',
+                'ExclusiveArch',
+                'ExclusiveOS',
+                'Group',
+                'License',
+                'Name',
+                'Obsoletes',
+                'Packager',
+                'Prereq',
+                'Provides',
+                'Recommends',
+                'Release',
+                'Requires',
+                'Suggests',
+                'Summary',
+                'Supplements',
+                'URL',
+                'Url',
+                'Vendor',
+                'Version',
+                /Patch\d*/,
+                /Source\d*/
+            ),
+        _value: ($) =>
+            choice(
+                $.simple_expansion,
+                $.expansion,
+                $.integer,
+                $.float,
+                seq($.float, optional($.simple_expansion)),
+                seq($.float, optional($.expansion)),
+                $.string
+            ),
 
         ///////////////////////////////////////////////////////////////////////
         // Description Section (%description)
@@ -121,31 +109,29 @@ module.exports = grammar({
         float: ($) => {
             const digits = repeat1(/[0-9]+_?/);
 
-            return token(seq(digits, '.', digits))
+            return token(seq(digits, '.', digits));
         },
 
-        string: $ => choice(
-            $._quoted_string,
-            $._unquoted_string,
-            seq("\\", NEWLINE)
-        ),
+        string: ($) =>
+            choice($._quoted_string, $._unquoted_string, seq('\\', NEWLINE)),
 
-        string_content: _ => token(prec(-1, /([^"`$\\\r\n]|\\(.|\r?\n))+/)),
+        string_content: (_) => token(prec(-1, /([^"`$\\\r\n]|\\(.|\r?\n))+/)),
 
-        _quoted_string: ($) => seq(
-            /["']/,
-            repeat(
-                seq(
-                    choice(
-                        seq(optional('%'), $.string_content),
-                        $.expansion,
-                        $.simple_expansion,
-                    ),
-                )
+        _quoted_string: ($) =>
+            seq(
+                /["']/,
+                repeat(
+                    seq(
+                        choice(
+                            seq(optional('%'), $.string_content),
+                            $.expansion,
+                            $.simple_expansion
+                        )
+                    )
+                ),
+                optional('%'),
+                /["']/
             ),
-            optional('%'),
-            /["']/,
-        ),
         _unquoted_string: ($) => $.string_content,
 
         ///////////////////////////////////////////////////////////////////////
@@ -153,28 +139,30 @@ module.exports = grammar({
         ///////////////////////////////////////////////////////////////////////
 
         variable_name: ($) => /[a-zA-Z0-9][\w\-]*/,
-        _simple_variable_name: $ => alias(/\w+/, $.variable_name),
-        _special_variable_name: $ => alias('?', $.special_variable_name),
+        _simple_variable_name: ($) => alias(/\w+/, $.variable_name),
+        _special_variable_name: ($) => alias('?', $.special_variable_name),
 
         // %variable
-        simple_expansion: ($) => seq(
-            '%',
-            choice(
-                $._simple_variable_name,
-                $._special_variable_name,
-                $.variable_name,
+        simple_expansion: ($) =>
+            seq(
+                '%',
+                choice(
+                    $._simple_variable_name,
+                    $._special_variable_name,
+                    $.variable_name
+                )
             ),
-        ),
 
         // %{variable}, %{?variable}
-        expansion: ($) => seq(
-            '%{',
-            choice(
-                $._simple_variable_name,
-                $._special_variable_name,
-                $.variable_name,
+        expansion: ($) =>
+            seq(
+                '%{',
+                choice(
+                    $._simple_variable_name,
+                    $._special_variable_name,
+                    $.variable_name
+                ),
+                '}'
             ),
-           '}',
-        ),
     },
 });
