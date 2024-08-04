78785b13bc44e5ebaa4e400e0c70b2c4c4c5c403
Andreas Schneider <asn@cryptomilk.org>
2024-08-04 13:09:39 +0200

    feat(queries): Add more highlight definitions

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -4,7 +4,7 @@
 [
   (tag)
   (dependency_tag)
-] @attribute
+] @property
 
 [
   (integer)
@@ -15,6 +15,40 @@
 (string) @string
 
 (if_statement) @keyword
+
+(macro_definition) @keyword.directive.define
+
+(macro_invocation) @keyword.function
+
+[
+  "%description"
+  "%package"
+  (files)
+  (changelog)
+] @function.macro
+
+[
+  (prep_scriptlet)
+  (generate_buildrequires)
+  (conf_scriptlet)
+  (build_scriptlet)
+  (install_scriptlet)
+  (check_scriptlet)
+  (clean_scriptlet)
+] @function.builtin
+
+[
+  "%artifact"
+  "%attr"
+  "%config"
+  "%dir"
+  "%doc"
+  "%docdir"
+  "%ghost"
+  "%license"
+  "%missingok"
+  "%readme"
+] @keyword.modifier
 
 ;[
 ;  "!="
@@ -31,5 +65,11 @@
   "%if"
   "%ifarch"
   "%ifos"
+  "%ifnarch"
+  "%ifnos"
+  "%elif"
+  "%elifarch"
+  "%elifos"
+  "%else"
   "%endif"
 ] @keyword.conditional
